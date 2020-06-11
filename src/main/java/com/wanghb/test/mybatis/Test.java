f034b27a8387b0a8e80dd8ef41ce8d9609709559
wanghb <wanghengbin@jiayuan.com>
2020-06-11 10:28:27 +0800

    update Test.java

--- src/main/java/com/wanghb/test/mybatis/Test.java
+++ src/main/java/com/wanghb/test/mybatis/Test.java
@@ -1,10 +1,17 @@
 package com.wanghb.test.mybatis;
 
 import com.wanghb.test.mybatis.mapper.SandboxMapper;
+import com.wanghb.test.mybatis.model.Sandbox;
+import tk.mybatis.mapper.entity.Condition;
+import tk.mybatis.mapper.entity.Example;
 
 public class Test {
 
     public static void main(String[] args) {
         SandboxMapper sandboxMapper;
+        Example example = new Example(Sandbox.class);
+        Example.Criteria criteria = example.createCriteria();
+        Condition condition = new Condition(Sandbox.class);
+
     }
 }
